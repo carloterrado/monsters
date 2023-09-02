3e3a1b9908023c244f70e0ca38d3c880490195a8
carloterrado <iamterradocarlo@gmail.com>
2023-09-02 14:51:31 +0800

    update pokemon id display

--- src/components/card-container/CardContainer.component.jsx
+++ src/components/card-container/CardContainer.component.jsx
@@ -3,7 +3,7 @@ import './card-container.css';
 
 const CardContainer = ({ monster }) => {
     const { name, id, sprites: { front_default }, types, base_experience } = monster;
-    const pokemonId = id + '';
+   
     const attributes = {
         water: '💧', fire: '🔥', grass: '🌿', poison: '💀', flying: '🦅', bug: '🐞', normal: '🐽', electric: '⚡', ground: '🌋', fairy: '🧚‍♀️', fighting: '🥊', psychic: '🔮', rock: '💎', steel: '🦾', ice: '🧊', ghost: '👻', dragon: '🐲', dark: '🌑',
     }
@@ -18,7 +18,7 @@ const CardContainer = ({ monster }) => {
             <h1 className="font-semibold text-2xl sm:text-4xl text-center "> {name}</h1>
             <img src={front_default} alt={`Pokemon ${name}`} />
             <p><em className='font-semibold text-sm flex justify-between'>
-                <span>{`#${pokemonId.padStart(4, '0')}`}</span> /
+                <span>{`#${(id + '').padStart(4, '0')}`}</span> /
                 <span>{pokemonTypes}</span> /
                 <span>{`${base_experience} exp`}</span>
             </em> </p>
